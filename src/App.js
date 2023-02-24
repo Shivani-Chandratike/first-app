b88837a0fa505039387bf5e970d2384959865c56
Shivani Chandratike <chandratike.shivani7@gmail.com>
2023-02-25 01:04:23 +0530

    my-button change backgroundcolor

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import { Button } from 'antd/es/radio';
+
+import 'antd/dist/reset.css'
 import './App.css';
-import React from 'react';
+import { Button } from 'antd'; 
+import { useState } from 'react';
+import { PoweroffOutlined } from '@ant-design/icons'; 
 
 
 function App() {
+  const [loading, setLoading] = useState(false)
+
+  const onButtonClick=(e) =>{
+    console.log('Button clicked')
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+
+    }, 2000)
+  }
   return (
     <div className="App">
-    <Button type="primary">Button</Button>
-    
-    
+      <header className="App-header">
+        <Button type='primary' 
+        block 
+        loading={loading}
+        icon={<PoweroffOutlined />}
+        className='my-button'
+        onClick={onButtonClick}>My First Button</Button>
+
+      </header>
     </div>
   );
 }
